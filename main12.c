31e461f19cb3a4ed3da917ad9576d04121a51be9
Fernanda Nolasco <104088221+paolafdanol@users.noreply.github.com>
2022-04-20 12:21:06 -0500

    Add files via upload

---
+++ main12.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int x;
+	for(x=0; x<=33; x++)
+	printf("%d\n", x*3+1);
+	return 0;
+}
